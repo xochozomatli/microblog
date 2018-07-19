2a24b8a1868800fe2ca980a92540a076a2532efc
Xochozomatli <aavillaverde11@gmail.com>
2018-07-18 19:15:59 -0600

    [Added following]

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
